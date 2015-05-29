2845c72c1802bd2e9f9972bbd1ab093ef00c20b4
Javier Santos <javier.santos.vel@gmail.com>
2015-05-29 13:22:53 +0200

    Update testAsignatura (C)

--- C/testAsignatura.c
+++ C/testAsignatura.c
@@ -8,9 +8,12 @@
 #include "asignatura.h"
 #include <stdio.h>
 
+void testGetAcronimo();
 void testMuestraAsignatura();
 
 int main (void) {
+	testGetAcronimo();
+
 	PAsignatura pa;
 	leeAsignaturaTeclado(pa);
 
@@ -19,14 +22,29 @@ int main (void) {
 	return 0;
 }
 
-void testMuestraAsignatura() {
-	Asignatura a1;
-	strcpy(a1.nombre, "Fundamentos de Programacion");
-	strcpy(a1.codigo, "123456ABC");
-	a1.creditos = 12.;
-	strcpy(a1.departamento, "LSI");
-	a1.tipo = ANUAL;
-	a1.curso = 1;
+void testGetAcronimo() {
+	Asignatura a;
+	Cadena res;
 
-	muestraAsignatura(a1);
+	strcpy(a.nombre, "Fundamentos de Programacion");
+	strcpy(a.codigo, "123456ABC");
+	a.creditos = 12.;
+	strcpy(a.departamento, "LSI");
+	a.tipo = ANUAL;
+	a.curso = 1;
+
+	getAcronimo(a, res);
+	printf("\tAcronimo: %s\n", res);
+}
+
+void testMuestraAsignatura() {
+	Asignatura a;
+	strcpy(a.nombre, "Fundamentos de Programacion");
+	strcpy(a.codigo, "123456ABC");
+	a.creditos = 12.;
+	strcpy(a.departamento, "LSI");
+	a.tipo = ANUAL;
+	a.curso = 1;
+
+	muestraAsignatura(a);
 }
